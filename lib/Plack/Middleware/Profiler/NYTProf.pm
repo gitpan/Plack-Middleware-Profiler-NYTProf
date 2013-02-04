24e3968223ee1ac5e53b254eded8647d3ae5af8a
dann <kitano.tk@gmail.com>
2013-02-04 13:29:21 +0000

    Import of KITANO/Plack-Middleware-Profiler-NYTProf-0.09 from CPAN.
    gitpan-cpan-distribution: Plack-Middleware-Profiler-NYTProf
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         KITANO/Plack-Middleware-Profiler-NYTProf-0.09.tar.gz
    gitpan-cpan-author:       KITANO
    gitpan-cpan-maturity:     released

--- lib/Plack/Middleware/Profiler/NYTProf.pm
+++ lib/Plack/Middleware/Profiler/NYTProf.pm
@@ -2,7 +2,7 @@ package Plack::Middleware::Profiler::NYTProf;
 use strict;
 use warnings;
 use parent qw(Plack::Middleware);
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 use Plack::Util::Accessor qw(
     enable_profile
@@ -97,15 +97,10 @@ sub call {
     my ( $self, $env ) = @_;
 
     $self->_setup_profiler unless $PROFILER_SETUPED{$$};
-
-    my $is_profiler_enabled = $self->enable_profile->($env);
-    if ($is_profiler_enabled) {
-        $env->{PROFILER_ENABLED} = 1;
-        $self->before_profile->( $self, $env );
-        $self->start_profiling($env);
-    }
+    $self->start_profiling_if_needed($env);
 
     my $res = $self->app->($env);
+
     if ( ref($res) && ref($res) eq 'ARRAY' ) {
         $self->stop_profiling_and_report_if_needed($env);
         return $res;
@@ -125,6 +120,16 @@ sub call {
             }
         }
     );
+}
+
+sub start_profiling_if_needed {
+    my ( $self, $env ) = @_;
+    my $is_profiler_enabled = $self->enable_profile->($env);
+    return unless $is_profiler_enabled;
+
+    $env->{PROFILER_ENABLED} = 1;
+    $self->before_profile->( $self, $env );
+    $self->start_profiling($env);
 }
 
 sub stop_profiling_and_report_if_needed {
